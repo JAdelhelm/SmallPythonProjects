cd89e94c1382d058fa14eee7bd880225a55805c0
JAdelhelm <83546217+JAdelhelm@users.noreply.github.com>
2021-07-02 13:35:08 +0200

    Update fileHandling.py

--- WordCounterTxtFile/fileHandling.py
+++ WordCounterTxtFile/fileHandling.py
@@ -1,10 +1,20 @@
 class fileHandling():
 
+"""
+Class to handle the file to read.
+Next step is to implement options to handle .txt files """
+
+dicOptions = {
+1: "Add String",
+2: "Add FilePath"
+}
+
     def __init__(self, filePath):
         self.filePath = filePath
 
     def readFile(self):
         if len(self.filePath) == 0:
+            optionsReadFile()
             self.filePath = str(input("Add filePath:\n"))
         try:
             contents = ""
@@ -15,3 +25,6 @@ class fileHandling():
             return contents
         except:
             print("FilePath is incorrect")
+
+    def optionsReadFile(self):
+        pass

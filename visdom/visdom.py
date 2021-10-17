d8d3d1cb34f4e007ab47a2962fced470a7c5d00e
Jörg Adelhelm <Joerg.adelhelm@gmx.de>
2021-10-17 17:41:53 +0200

    Update

--- visdom/visdom.py
+++ visdom/visdom.py
@@ -2,49 +2,7 @@ import re
 from pathlib import Path
 import collections
 import matplotlib.pyplot as plt
-
-
-class visualizeData():
-    def __init__(self,dataset, numWords=20):
-        self.dataset = dataset
-        self.numWords = numWords
-        self.sumOfAllWords = sum(self.dataset.values())
-        print(f"Sum of all words: ",self.sumOfAllWords)
-        self.wordList = []
-        self.numList = []
-        countMostCommon = collections.Counter(self.dataset)
-        self.dataset = countMostCommon.most_common(numWords)
-        self.getArrays(self.dataset)  
-        self.plot_set()
-
-    def plot_set(self):
-        print(f"First {self.numWords} sorted Words: {self.dataset}")
-        fig, ax = plt.subplots()
-        options = str(input("Do you like to see the results in percent? (Y/N): ")).upper()
-        if options == "Y":
-            percentageData = [(val/self.sumOfAllWords)*100 for val in self.numList]
-            ax.plot(self.wordList[: self.numWords],percentageData[:self.numWords])
-            ax.set_title(f"Most {self.numWords} frequent words")
-            ax.set_ylabel("Percentage")
-            ax.set_xlabel("Words")
-            ax.tick_params(axis='both',labelsize=8)
-            plt.show()
-        elif options == "N":
-            ax.plot(self.wordList[:self.numWords],self.numList[:self.numWords])
-            ax.set_title(f"Most {self.numWords} frequent words")
-            ax.set_ylabel("Words")
-            ax.set_xlabel("Frequence")
-            ax.tick_params(axis='both',labelsize=8)
-            plt.show()
-        else:
-            print("No option was chosen")
-
-    def getArrays(self, listOfWords):
-        for i in listOfWords:
-            for j, k  in listOfWords:
-                self.wordList.append(j)
-                self.numList.append(k)
-            break
+from vis import visualizeData
 
 class visdom():
     def __init__(self):
@@ -62,7 +20,6 @@ class visdom():
             except:
                 print("FilePath is incorrect")
                 raise NameError
-
     def prepare(self, content):
         try:
             content = content.lower()
@@ -75,7 +32,6 @@ class visdom():
 visObject = visdom()
 content = visObject.readTxt()
 content = visObject.prepare(content)
-
 # Visualize
 newVisualization = visualizeData(dataset=content,numWords=5)
 
